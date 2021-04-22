8ba3b2c75780888896089b58d5eeffccb0b804ee
Tim Vigneau <tjvig94@gmail.com>
2021-04-22 18:50:56 -0400

    Updated file structure

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/styles.css',
-    '/index.js',
+    '/assets/css/styles.css',
+    '/assets/js/index.js',
     '/manifest.webmanifest',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
